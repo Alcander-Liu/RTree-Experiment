eff0e5255fa51a9197da7f2bd6704115971d00c7
alcander_liu <chiawei.liu.1201@gmail.com>
2018-05-28 16:00:06 +0800

    add output result

--- src/query.cpp
+++ src/query.cpp
@@ -30,19 +30,27 @@ int main()
   
   std::ifstream query(queryFile);
   std::ifstream feature(featureFile);
-  std::ifstream imageList("imagelist.txt");
+  std::ifstream imageList("./input/imagelist.txt");
+  std::ofstream outputResult("./output/result.txt", std::ofstream::app);
+
+  outputResult << "Dimension: " << NUMDIMS << std::endl;
+  outputResult << "query file: " << queryFile << std::endl;
+  outputResult << "feature file: " << featureFile << std::endl;
+  outputResult << "data amount: " << dataAmount << std::endl;
+  outputResult << "Range: " << range << std::endl;
 
   // build two way index list of image names
   std::string * imageNames = new std::string[dataAmount];
   std::string imageName;
   std::map<std::string, int> nameToIndex;
-  for(int i = 0; imageList.good(); i++)
+  for(int i = 0; i < dataAmount; i++)
   {
+    //std::cout << "read image name" << std::endl;
     getline(imageList, imageName);
     imageNames[i] = imageName;
     nameToIndex.emplace(imageNames[i], i);
   }
-
+  //std::cout << "list built" << std::endl;
   // build R tree
   int **features = new int*[dataAmount];
   RTree<int, int, NUMDIMS, float> tree;
@@ -52,11 +60,12 @@ int main()
     for(int i = 0; i < NUMDIMS; i++)
       feature >> features[j][i];
     tree.Insert(features[j], features[j], j);
+    //std::cout << "log" << std::endl;
   }
 
   std::string queryName;
   std::vector<int> ids; //store search results
-  int queryIndex, touchCount;
+  int queryIndex, touchCount, touchCountSum = 0, searchAmount = 0, resultAmount = 0;
   int *queryRectMin = new int[NUMDIMS];
   int *queryRectMax = new int[NUMDIMS];
 
@@ -70,14 +79,18 @@ int main()
       queryRectMin[i] = features[queryIndex][i] - range;
     }
     touchCount = tree.Search(queryRectMin, queryRectMax, MySearchCallback, &ids);
+    touchCountSum += touchCount;
+    resultAmount += ids.size();
+    searchAmount++;
     std::cout << queryName <<"  search results: " << std::endl;
     std::cout << "  Node touched times: " << touchCount << std::endl;
     for(int i = 0; i < ids.size(); i++)
       std::cout << "  " << imageNames[ids[i]] << std::endl;
     ids.clear();
   }
-  
-  
-
+  std::cout << "Average touched times per search: " << float(touchCountSum)/searchAmount << std::endl;
+  std::cout << "Average results found per search: " << float(resultAmount)/searchAmount << std::endl;
+  outputResult << "Average touched times per search: " << float(touchCountSum)/searchAmount << std::endl;
+  outputResult << "Average results found per search: " << float(resultAmount)/searchAmount << std::endl << std::endl;
   return 0;
 }
